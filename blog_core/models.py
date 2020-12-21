9262f556f91512c1ebd15c31d0e3d68e76ed9fde
mstrpol <mstrpol@yandex.ru>
2020-12-21 11:37:46 +0700

    add blog_core: - Alter field image on post

--- blog_core/models.py
+++ blog_core/models.py
@@ -14,6 +14,10 @@ class Post(models.Model):
     rating = models.IntegerField(verbose_name='Рейтинг')
     is_publish = models.BooleanField(default=False, verbose_name='Опубликовано')
     date_publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    image = models.ImageField(
+        default='/static/assets/images/blog/blog-post-thumb-1.jpg',
+        upload_to='static/assets/images/blog/'
+    )
 
     def __str__(self):
         return '{}: {}'.format(self.author, self.title)
